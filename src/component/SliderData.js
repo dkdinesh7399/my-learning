5e94797708dcedf5cb2c3b6546c09402f1ab9dcd
dkdinesh7399 <dkdinesh7398@gmail.com>
2024-08-14 20:49:21 +0530

    initial commit msg

---
+++ src/component/SliderData.js
@@ -0,0 +1,19 @@
+export const SliderData=[
+    {
+    Image:
+         'https://images.pexels.com/photos/218863/pexels-photo-218863.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+},
+{
+    Image:
+        'https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+},
+{
+    Image:
+
+        'https://images.pexels.com/photos/1586973/pexels-photo-1586973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+},
+{
+    Image:
+        'https://images.pexels.com/photos/1467277/pexels-photo-1467277.jpeg?auto=compress&cs=tinysrgb&w=400'
+}
+];
---
+++ src/component/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import pic1 from 'pic1.jpg'
+
+const navbar = () => {
+  return <nav className=' bg-red-900 mb-20 flex items-center justify-between py-6'>
+    <div className='flex flex-shrink-0 items-center'>
+<img src='pic1.jpg' alt='laptop'/>
+
+    </div>
+    </nav>
+        
+  
+}
+
+export default navbar
