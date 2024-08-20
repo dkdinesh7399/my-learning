8adf2348c0babe0c36be4e8f86eb94b0b09660b3
dkdinesh7399 <dkdinesh7398@gmail.com>
2024-08-20 20:08:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageSlider from './component/ImageSlider';
+import { SliderData } from './component/SliderData';
+
+function App() {
+  return (
+    <ImageSlider 
+    Slides ={SliderData}
+    />
+);}
+
+export default App;
